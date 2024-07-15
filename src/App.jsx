85e91a6b0032a846de74947eec5a1783f57d514b
Eyvlina <evylina5@gmail.com>
2024-07-15 13:40:36 +0100

    added some styling to the quiz component

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App overflow-hidden">
       <header className="bg-primary h-3/6">
-        <h1 class ='py-6 text-7xl text-white font-serif'>Capital Conqueror</h1>
+        <h1 class ='py-6 text-5xl text-white font-serif'>Capital Conqueror</h1>
       </header>
       <StartScreen />
       <Quiz />
--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -20,17 +20,19 @@ export default function Quiz() {
     fetchQuizData();
   };
 
+  
+
   return (
-    <div className="mt-10">
+    <div className="mt-10 py-4 w-3/4 mx-auto backdrop-opacity-5">
       {showQuiz && (
         <>
         <div className="text-center">
-          <h2 className="text-2xl font-bold">{country}</h2>
+          <h2 className="text-3xl font-bold text-warning">{country.toUpperCase()}</h2>
           <ul className="mt-4">
             {shuffledCapitals.map((capital, index) => (
               <li key={index}>
                 <button
-                  className="btn btn-lg mt-2 w-3/4 "
+                  className="btn btn-lg mt-2 w-3/4 bg-secondary text-white"
                   onClick={() => handleResponse(capital)}
                 >
                   {capital}
@@ -39,22 +41,24 @@ export default function Quiz() {
             ))}
           </ul>
         </div>
-      
+    
+
+      <button
+        className="btn btn-md mx-auto mt-5 w-2/4 bg-primary text-white"
+        onClick={handleNextCountry}
+      >
+        Next Country
+      </button>
+
       {result && (
         <p
-          className={`mt-4 text-lg ${
+          className={`mt-5 text-3xl ${
             result.includes("CORRECT!") ? "text-accent" : "text-error"
           }`}
         >
           {result}
         </p>
       )}
-      <button
-        className="btn btn-primary btn-sm mx-auto mt-10 w-2/4"
-        onClick={handleNextCountry}
-      >
-        Next Country
-      </button>
       </>
       )}
     </div>
