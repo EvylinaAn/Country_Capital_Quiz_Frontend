c930c7f4d3375a4684a33c43aed9ebcb0ca2c167
Eyvlina <evylina5@gmail.com>
2024-07-17 17:39:34 +0100

    ready for deploy

--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -6,64 +6,67 @@ export default function Quiz() {
     useQuiz();
 
   const [result, setResult] = useState("");
-  const [selected, setSelected] = useState(false)
+  const [selected, setSelected] = useState(false);
+
+  
 
   const handleResponse = (selectedResponse) => {
-    setSelected(true)
+    setSelected(true);
     if (selectedResponse === correctCapital) {
       setResult("CORRECT!");
     } else {
-      setResult(`Wrong! The correct answer is ${correctCapital.toUpperCase()}`);
+      setResult(
+        `Wrong! The correct answer is ${correctCapital.toUpperCase()}`
+      );
     }
   };
 
   const handleNextCountry = () => {
     setResult("");
-    setSelected(false)
+    setSelected(false);
     fetchQuizData();
   };
-
-  
 
   return (
     <div className="mt-10 py-4 w-3/4 mx-auto backdrop-opacity-5">
       {showQuiz && (
         <>
-        <div className="text-center">
-          <h2 className="text-3xl font-bold text-warning">{country.toUpperCase()}</h2>
-          <ul className="mt-4">
-            {shuffledCapitals.map((capital, index) => (
-              <li key={index}>
-                <button
-                  className="btn btn-lg mt-2 w-3/4 bg-secondary text-white"
-                  onClick={() => handleResponse(capital)}
-                  disabled={selected}
-                >
-                  {capital}
-                </button>
-              </li>
-            ))}
-          </ul>
-        </div>
-    
+          <div className="text-center">
+            <h2 className="text-3xl font-bold text-warning">
+              {country.toUpperCase()}
+            </h2>
+            <ul className="mt-4">
+              {shuffledCapitals.map((capital, index) => (
+                <li key={index}>
+                  <button
+                    className="btn btn-lg mt-2 w-3/4 bg-secondary text-white"
+                    onClick={() => handleResponse(capital)}
+                    disabled={selected}
+                  >
+                    {capital}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
 
-      <button
-        className="btn btn-md mx-auto mt-5 w-2/4 bg-primary text-white"
-        onClick={handleNextCountry}
-      >
-        Next Country
-      </button>
+          <button
+            className="btn btn-md mx-auto mt-5 w-2/4 bg-primary text-white"
+            onClick={handleNextCountry}
+          >
+            Next Country
+          </button>
 
-      {result && (
-        <p
-          className={`mt-5 text-3xl ${
-            result.includes("CORRECT!") ? "text-accent" : "text-error"
-          }`}
-        >
-          {result}
-        </p>
-      )}
-      </>
+          {result && (
+            <p
+              className={`mt-5 text-3xl ${
+                result.includes("CORRECT!") ? "text-accent" : "text-error"
+              }`}
+            >
+              {result}
+            </p>
+          )}
+        </>
       )}
     </div>
   );
